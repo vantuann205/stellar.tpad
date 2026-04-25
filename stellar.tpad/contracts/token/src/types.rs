02b51c94b2bc656601316941db652308a54a13f9
vantuann205 <vantuan132005@gmail.com>
2026-04-25 11:34:28 +0700

    feat: add token types and errors

--- stellar.tpad/contracts/token/src/types.rs
+++ stellar.tpad/contracts/token/src/types.rs
@@ -1,1 +1,40 @@
-// Token domain types
+use soroban_sdk::{contracttype, Address};
+
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+    Admin,
+    Decimals,
+    Name,
+    Symbol,
+    Sac,
+    Balance(Address),
+    Allowance(AllowanceKey),
+}
+
+#[derive(Clone)]
+#[contracttype]
+pub struct AllowanceKey {
+    pub from: Address,
+    pub spender: Address,
+}
+
+#[derive(Clone)]
+#[contracttype]
+pub struct AllowanceValue {
+    pub amount: i128,
+    pub expiration_ledger: u32,
+}
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum TokenError {
+    AlreadyInitialized      = 1,
+    NotInitialized          = 2,
+    Unauthorized            = 3,
+    InvalidAmount           = 4,
+    InsufficientBalance     = 5,
+    InsufficientAllowance   = 6,
+    InvalidExpirationLedger = 7,
+}
